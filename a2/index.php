669a7021fd660e42c6c1777122e4d0a28d5620d2
kevinvu184 <s3678490@rmit.edu.vn>
2019-08-08 14:07:26 +1000

    Modify placeholder text

--- a2/index.php
+++ a2/index.php
@@ -31,7 +31,7 @@
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Kevin Vu, s3678490 and A2-s1234567-s1234568. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
